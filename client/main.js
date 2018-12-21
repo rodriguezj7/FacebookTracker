0729601f970c2d94d02c5bc190ace484f6529b32
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-12-21 17:26:42 -0500

    working on importing firbase and fixing rendering of html on dom

--- client/main.js
+++ client/main.js
@@ -7,18 +7,29 @@ class App {
     this.checkIfIssuesExist = this.checkIfIssuesExist.bind(this);
     this.fetchIssues = this.fetchIssues.bind(this);
     this.deleteIssue = this.deleteIssue.bind(this);
+    this.deleteHtml = this.deleteHtml.bind(this);
+    this.initializeEventListeners = this.initializeEventListeners.bind(this);
   }
-  setStatusClosed(id) {
+  initializeEventListeners() {
+    document
+      .getElementById("close")
+      .addEventListener("submit", InitApp.setStatusClosed(`${this.id}`));
+
+    document.getElementById('delete').addEventListener("submit", InitApp.deleteIssue(`${this.id}`));
+  }
+  setStatusClosed(e, id) {
+    e.preventDefault();
     for (let issue of this.issues) {
-      issue.id == id ? (issue.status = "Closed") : null;
+      (issue.id === id) ? (issue.status = "Closed") : null;
+      this.deleteHtml(id)
       this.fetchIssues();
     }
   }
   //createissue
   createIssue(event) {
     event.preventDefault();
-    const newIssue = new Issue();
-    this.saveIssue(newIssue.issue);
+    const NewIssue = new Issue();
+    this.saveIssue(NewIssue.issue);
     document.getElementById("issueInputForm").reset();
     this.fetchIssues();
   }
@@ -41,24 +52,38 @@ class App {
       this.createHtml(issue);
     }
   }
+  // remove html
+  deleteHtml(id) {
+    var divToRemove = document.getElementById(`${id}`);
+    divToRemove.remove(); // Removes the div with the appropriate id
+  }
   //method for creating html
-  createHtml({ id, description, job, assignedTo, status } = {}) {
+  createHtml({
+    id,
+    description,
+    job,
+    assignedTo,
+    status
+  } = {}) {
     const issuesList = document.getElementById("issuesList");
+    issuesList.innerHTML += '';
     const issueCard = `<div class="well" id="${id}">
                                 <h6>Issue ID:  ${id} </h6>
                                 <p><span class="label label-info"> ${status} </span></p>
                                 <h3> ${description} </h3>
                                 <p><span class="glyphicon glyphicon-globe"></span> <a href="${job}">${job}</a></p>
                                 <span class="glyphicon glyphicon-user"></span> ${assignedTo} </p>
-                                <a href="#" class="btn btn-warning" onclick="InitApp.setStatusClosed(${id})">Close</a>
-                                <a href="#" class="btn btn-danger" onclick="InitApp.deleteIssue(${id})">Delete</a>
+                                <a href="#" class="btn btn-warning" id="close" onclick="InitApp.setStatusClosed('${id}')">Close</a>
+                                <a href="#" class="btn btn-danger"id="delete" onclick="InitApp.deleteIssue('${id}')">Delete</a>
                                 </div>`;
     issuesList.innerHTML += issueCard;
   }
   deleteIssue(id) {
     for (let i = 0; i < this.issues.length; i++) {
       if (this.issues[i].id === id) {
-        this.issues[i].splice(i, 1);
+        console.log(this.issues[i].id);
+        this.issues.splice(i, 1);
+        this.deleteHtml(id);
       }
     }
     this.fetchIssues();
@@ -68,7 +93,7 @@ class App {
 class Issue extends App {
   constructor() {
     super();
-    this.id = chance.guid();
+    this.id = `${chance.guid()}`;
     this.desc = document.getElementById("issueDescInput").value;
     this.job = document.getElementById("issueJobInput").value;
     this.assignedTo = document.getElementById("issueAssignedToInput").value;
@@ -81,9 +106,11 @@ class Issue extends App {
       status: this.status
     };
   }
+
 }
 
 const InitApp = new App();
 document
   .getElementById("issueInputForm")
   .addEventListener("submit", InitApp.createIssue);
+InitApp.initializeEventListeners();
