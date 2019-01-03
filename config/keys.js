9151ae912b920c30f3c5bf69979aa4957950007c
clubzspadez <clubzspadez@gmail.com>
2019-01-02 22:38:09 -0500

    removing mongo since im working with firebase

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-module.exports = {
-  mongoURI: "mongodb://admin:Jryellow4856@ds135714.mlab.com:35714/tracking",
-  secret: "secret"
-};
+// module.exports = {
+//   mongoURI: "mongodb://admin:Jryellow4856@ds135714.mlab.com:35714/tracking",
+//   secret: "secret"
+// };
